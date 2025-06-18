41a3ceadcdfe367c761e23e48f3c80248601c23d
Lexag <lexa.grebnets@gmail.com>
2025-06-18 12:33:01 +0200

    move command to common to allow network commands

---
+++ src/command.rs
@@ -0,0 +1,51 @@
+use std::error::Error;
+use std::fmt;
+
+use serde::{Deserialize, Serialize};
+
+use crate::cue::Cue;
+
+#[derive(Clone, Deserialize, Serialize)]
+pub enum ControlCommand {
+    TransportStart,
+    TransportStop,
+    TransportZero,
+    LoadCue(Cue),
+}
+
+impl fmt::Debug for ControlCommand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ControlCommand::TransportZero => write!(f, "TransportZero"),
+            ControlCommand::TransportStart => write!(f, "TransportStart"),
+            ControlCommand::TransportStop => write!(f, "TransportStop"),
+            ControlCommand::LoadCue(_cue) => write!(f, "LoadCue"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum CommandError {
+    UnknownCommand,
+    IsRunning,
+    IsNotRunning,
+}
+
+impl fmt::Display for CommandError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CommandError::UnknownCommand => write!(f, "Unknown command"),
+            _ => {
+                write!(f, "")
+            }
+        }
+    }
+}
+
+impl Error for CommandError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            _ => None,
+        }
+    }
+}
