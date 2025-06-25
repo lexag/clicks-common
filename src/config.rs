378e4b597687b5c8620bde7c97e69272146895a0
Lexag <lexa.grebnets@gmail.com>
2025-06-25 11:17:25 +0200

    audio config from external

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct AudioConfiguration {
-    server: JACKServerConfiguration,
-    client: JACKClientConfiguration,
+    pub server: JACKServerConfiguration,
+    pub client: JACKClientConfiguration,
 }
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct JACKServerConfiguration {}
+pub struct JACKServerConfiguration {
+    pub device_name: String,
+    pub system_name: String,
+    pub sample_rate: usize,
+    pub num_channels: usize,
+}
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct JACKClientConfiguration {}
+pub struct JACKClientConfiguration {
+    pub name: String,
+    pub num_channels: usize,
+}
