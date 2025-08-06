5b09eefc8b69f8e66a061888888e720819cf6cae
Lexag <lexa.grebnets@gmail.com>
2025-08-06 18:02:35 +0200

    separate States in Notification from ProcessorStatus

--- src/status.rs
+++ src/status.rs
@@ -1,64 +1,90 @@
-use std::fmt::{Debug, Display};
-
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    command::ControlCommand,
     config::SystemConfiguration,
     cue::{Beat, Cue},
-    network::{ConnectionInfo, Heartbeat, JACKStatus, NetworkStatus, SubscriberInfo},
+    network::{Heartbeat, JACKStatus, NetworkStatus},
     show::Show,
     timecode::TimecodeInstant,
 };
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct PlaybackStatus {
+pub struct PlaybackState {
     pub clip_idx: usize,
     pub current_sample: i32,
     pub playing: bool,
     pub clips: Vec<usize>,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct BeatStatus {
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct BeatState {
     pub beat_idx: usize,
     pub next_beat_idx: usize,
-    pub us_to_next: usize,
+    pub beat: Beat,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct CueState {
+    pub cue_idx: usize,
+    pub cue: Cue,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum AudioSourceStatus {
-    BeatStatus(BeatStatus),
+pub enum AudioSourceState {
+    BeatStatus(BeatState),
     TimeStatus(TimecodeInstant),
-    PlaybackStatus(PlaybackStatus),
+    PlaybackStatus(PlaybackState),
     None,
 }
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct ProcessStatus {
-    pub sources: Vec<AudioSourceStatus>,
-    pub running: bool,
-    pub beat_idx: usize,
-    pub next_beat_idx: usize,
+#[derive(Clone, Debug, Serialize, Deserialize, Default)]
+pub struct TransportState {
     pub us_to_next_beat: usize,
-    pub time: TimecodeInstant,
-    pub cue_idx: usize,
+    pub running: bool,
 }
 
-#[derive(Default)]
+//#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+//pub struct ProcessStatus {
+//    pub sources: Vec<AudioSourceState>,
+//    pub running: bool,
+//    pub beat_idx: usize,
+//    pub next_beat_idx: usize,
+//    pub us_to_next_beat: usize,
+//    pub time: TimecodeInstant,
+//    pub cue_idx: usize,
+//}
+
+#[derive(Default, Clone, Debug)]
 pub struct CombinedStatus {
-    pub process_status: ProcessStatus,
-    pub cue: Cue,
+    pub sources: Vec<AudioSourceState>,
+    pub transport: TransportState,
+    pub cue: CueState,
     pub show: Show,
     pub network_status: NetworkStatus,
     pub jack_status: JACKStatus,
+}
+
+impl CombinedStatus {
+    pub fn beat_state(&self) -> BeatState {
+        if let AudioSourceState::BeatStatus(state) = &self.sources[0] {
+            state.clone()
+        } else {
+            panic!("Metronome is not in slot 0")
+        }
+    }
+    pub fn time_state(&self) -> TimecodeInstant {
+        if let AudioSourceState::TimeStatus(state) = &self.sources[1] {
+            state.clone()
+        } else {
+            panic!("Timecode is not in slot 1")
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum NotificationKind {
     TransportChanged,
     BeatChanged,
-    PlaystateChanged,
     CueChanged,
     ShowChanged,
     NetworkChanged,
@@ -68,12 +94,11 @@ pub enum NotificationKind {
     Heartbeat,
 }
 
-#[derive(Deserialize, Serialize, Clone)]
+#[derive(Deserialize, Serialize, Clone, Debug)]
 pub enum Notification {
-    TransportChanged(ProcessStatus),
-    BeatChanged(usize, Beat),
-    PlaystateChanged(bool),
-    CueChanged(usize, Cue),
+    TransportChanged(TransportState),
+    BeatChanged(BeatState),
+    CueChanged(CueState),
     ShowChanged(Show),
     NetworkChanged(NetworkStatus),
     JACKStateChanged(JACKStatus),
@@ -87,7 +112,6 @@ impl Notification {
         match self {
             Self::TransportChanged(..) => NotificationKind::TransportChanged,
             Self::BeatChanged(..) => NotificationKind::BeatChanged,
-            Self::PlaystateChanged(..) => NotificationKind::PlaystateChanged,
             Self::CueChanged(..) => NotificationKind::JACKStateChanged,
             Self::ShowChanged(..) => NotificationKind::CueChanged,
             Self::NetworkChanged(..) => NotificationKind::ShowChanged,
@@ -95,27 +119,6 @@ impl Notification {
             Self::ConfigurationChanged(..) => NotificationKind::ConfigurationChanged,
             Self::ShutdownOccured => NotificationKind::ShutdownOccured,
             Self::Heartbeat(..) => NotificationKind::Heartbeat,
-        }
-    }
-}
-
-impl Debug for Notification {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        match self {
-            Notification::TransportChanged(status) => {
-                write!(f, "{status:?}")
-            }
-            Notification::CueChanged(idx, cue) => write!(f, "{cue:?}"),
-            Notification::ConfigurationChanged(status) => {
-                write!(f, "{status:?}")
-            }
-            Notification::ShowChanged(status) => write!(f, "{status:?}"),
-            Notification::NetworkChanged(status) => {
-                write!(f, "{status:?}")
-            }
-            Notification::JACKStateChanged(status) => write!(f, "{status:?}"),
-            Notification::Heartbeat(heartbeat) => write!(f, "{heartbeat:?}"),
-            _ => write!(f, "Unimplemented representation."),
         }
     }
 }
