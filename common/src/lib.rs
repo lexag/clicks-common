c602ebd981f3edb665e5556e15f8f21225519e11
Lexag <lexa.grebnets@gmail.com>
2025-10-25 22:26:59 +0200

    top-level documentation

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,6 +1,18 @@
+#![warn(missing_docs)]
+//! This crate contains common functionality and data formats shared between the ClicKS core and
+//! client components
+
+/// Definition of beat, the lowest-level timing unit in a Cue definition
 pub use beat;
+/// Definitions of cue and show
 pub use cue;
+/// Event types
 pub use event;
+/// Definitions pertaining to local core storage that other clients might still have use for, such
+/// as configuration and status
 pub use local;
+/// Low-level data representation formats
 pub use mem;
+/// Defines the data format for the network protocol used to communicate between
+/// between the ClicKS core and ClicKS clients.
 pub use protocol;
--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,2 +1,5 @@
+/// Definition of messages sent core -> client
 pub mod message;
+
+/// Definition of requests send client -> core
 pub mod request;
