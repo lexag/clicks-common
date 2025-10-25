ec2957a53c3610a485b4b3cb487955dc31abb5f5
Lexag <lexa.grebnets@gmail.com>
2025-10-25 23:42:23 +0200

    document beat/

--- beat/src/beat.rs
+++ beat/src/beat.rs
@@ -1,10 +1,18 @@
 use core::fmt;
 use serde::{Deserialize, Serialize};
 
+/// Beat represent a musical beat, or a subdivision thereof
 #[derive(Clone, Serialize, Deserialize, Default, PartialEq, Copy)]
 pub struct Beat {
+    /// Beat number, first beat in a bar typically has 1, followed by 2 etc. Can be between 0 and 255.
     pub count: u8,
+    /// Bar number, first bar in a cue typically has 1, followed by 2 etc. Can be between 0 and
+    /// 255.
     pub bar_number: u8,
+    /// Length of this beat in microseconds. 0-4_294_967_295 (u32) means tempos between one beat
+    /// per microsecond and one beat per approx 1h 11m.
+    /// In practice, the top speed of beat processing is limited by core performance, and a beat
+    /// processing speed of 1MHz is probably unlikely.
     pub length: u32,
 }
 
@@ -18,6 +26,10 @@ impl fmt::Debug for Beat {
 }
 
 impl Beat {
+    /// Returns an empty Beat
+    /// Does not have any length, and is thus unstable for playback.
+    /// Should not be used to directly generate played Beats, just to fill out collections with
+    /// beats to be edited.
     pub fn empty() -> Beat {
         Beat {
             count: 0,
@@ -26,17 +38,19 @@ impl Beat {
         }
     }
 
-    //pub fn events_filter<F>(&self, filter: F) -> Vec<BeatEvent>
-    //where
-    //    F: Fn(&BeatEvent) -> bool,
-    //{
-    //    self.events.iter().filter(|e| filter(e)).cloned().collect()
-    //}
-
+    /// Tempo this beat implies in BPM.
+    /// Note that this does not necessary correlate to pulse, as a pulse-beat may be subdivided
+    /// into multiple metronome beats.
     pub fn tempo(&self) -> u16 {
         if self.length == 0 {
             return 0;
         }
         (60000000.0 / self.length as f32).round() as u16
+    }
+
+    /// Is this beat null (opposite of populated), i.e. is this beat just an empty slot in the beat
+    /// table (true), or an actual beat (false)?
+    pub fn is_null(&self) -> bool {
+        self.length == 0 && self.bar_number == 0 && self.count == 0
     }
 }
--- beat/src/lib.rs
+++ beat/src/lib.rs
@@ -1,2 +1,4 @@
+#![warn(missing_docs)]
+//! Definition of beat, the lowest-level timing unit in a Cue definition
 mod beat;
 pub use beat::Beat;
