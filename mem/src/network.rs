c6dc775fe804e949fd9d1589bd01511c4b6c00d8
Lexag <lexa.grebnets@gmail.com>
2025-10-29 20:37:39 +0100

    IpAddress access and constructor functions

--- mem/src/network.rs
+++ mem/src/network.rs
@@ -1,4 +1,5 @@
 use crate::{message::MessageType, str::String32};
+use core::fmt;
 use serde::{Deserialize, Serialize};
 
 /// Information about a client subscribing to core messages
@@ -17,8 +18,58 @@ pub struct SubscriberInfo {
 /// Ipv4 address and udp port information
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Default, Eq)]
 pub struct IpAddress {
-    port: u16,
-    addr: [u8; 4],
+    /// UDP/TCP port number 0-65535
+    pub port: u16,
+    /// Ipv4 address octets [192, 168, 1, 150]
+    pub addr: [u8; 4],
+}
+
+impl IpAddress {
+    /// Create a new IpAddress from four address octets and a port number
+    pub fn new(address: [u8; 4], port: u16) -> Self {
+        Self {
+            port,
+            addr: address,
+        }
+    }
+
+    /// Create a new IpAddress from a typical ip string format "192.168.1.150:3030"
+    pub fn from_address_str(str: &str) -> Option<Self> {
+        let (addr, port) = str.split_once(':')?;
+        Some(Self {
+            port: port.parse().ok()?,
+            addr: IpAddress::octets_from_str(addr)?,
+        })
+    }
+
+    /// Create a new IpAddress from a typical ip string ("192.168.1.150") and a separate port
+    /// number     
+    pub fn from_str_and_port(str: &str, port: u16) -> Option<Self> {
+        Some(Self {
+            port,
+            addr: IpAddress::octets_from_str(str)?,
+        })
+    }
+
+    fn octets_from_str(mut str: &str) -> Option<[u8; 4]> {
+        let mut octets: [u8; 4] = [0; 4];
+        for i in &mut octets {
+            let (octet, rest) = str.split_once('.')?;
+            *i = octet.parse().ok()?;
+            str = rest;
+        }
+        Some(octets)
+    }
+}
+
+impl fmt::Display for IpAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}.{}.{}.{}:{}",
+            self.addr[0], self.addr[1], self.addr[2], self.addr[3], self.port
+        )
+    }
 }
 
 /// Which end of a network connection is this?
