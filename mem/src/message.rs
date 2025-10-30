d56886d57c69d4db1fc7cc5c60b9eda02a2e4e5b
Lexag <lexa.grebnets@gmail.com>
2025-10-30 10:55:44 +0100

    fix numbering error in MessageType

--- mem/src/message.rs
+++ mem/src/message.rs
@@ -3,24 +3,24 @@ use serde::{Deserialize, Serialize};
 bitflags::bitflags! {
     /// Bitflag for one or multiple types of core -> client message
     #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
-    pub struct MessageType: u8 {
+    pub struct MessageType: u16 {
         /// Transport changed
         const TransportData = 0x01;
         /// Beat changed
-        const BeatData = 0x01;
+        const BeatData = 0x02;
         /// Cue changed
-        const CueData = 0x02;
+        const CueData = 0x04;
         /// Show changed
-        const ShowData = 0x04;
+        const ShowData = 0x08;
         /// Network changed
-        const NetworkChanged = 0x08;
+        const NetworkChanged = 0x10;
         /// JACKState changed
-        const JACKStateChanged = 0x10;
+        const JACKStateChanged = 0x20;
         /// Configuration changed
-        const ConfigurationChanged = 0x20;
+        const ConfigurationChanged = 0x40;
         /// Shutdown occured
-        const ShutdownOccured = 0x40;
+        const ShutdownOccured = 0x80;
         /// Heartbeat
-        const Heartbeat = 0x80;
+        const Heartbeat = 0x100;
     }
 }
