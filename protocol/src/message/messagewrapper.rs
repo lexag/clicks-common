9f27946f22266728bf2d1662f82538d3063c297d
Lexag <lexa.grebnets@gmail.com>
2025-11-21 13:04:22 +0100

    add serde and bincode to Message wrapper

--- protocol/src/message/messagewrapper.rs
+++ protocol/src/message/messagewrapper.rs
@@ -2,6 +2,8 @@ use crate::message::{LargeMessage, SmallMessage};
 
 /// Wrapper type for both types of core -> client messages
 #[allow(clippy::large_enum_variant)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Debug, bincode::Encode, bincode::Decode)]
 pub enum Message {
     /// Small, const-size, uC-friendly status updates
     Small(SmallMessage),
